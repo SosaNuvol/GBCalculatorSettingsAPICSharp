e6b6240c1d110634edf5ab46877ae3f3060765ff
SosaUGOV <aesosa@utah.gov>
2024-12-07 18:32:04 -0700

    Save working Cache mechanism.

--- GBCalculatorRatesAPI/Business/Models/CacheModels..cs
+++
@@ -1,8 +0,0 @@
-// using Newtonsoft.Json;
-
-// namespace GBCalculatorRatesAPI.Business.Models;
-
-// public class CurrencyExchangeCacheModel {
-// 	[JsonProperty("id")]
-// 	public string Id { get; set; }
-// }
